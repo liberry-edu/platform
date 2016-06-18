ea91bdb64a83939ad90a99e25a9bc1c56cc203ac
Vaibhav <vaibhavsinh@gmail.com>
2016-06-18 12:55:29 +0530

    Deleting app before restarting to ensure fresh code directory is read

--- scripts/pi/restart.sh
+++ scripts/pi/restart.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-rm -rf $LIBERRY_ROOT/code
-cp -r $1/code $LIBERRY_ROOT/
+rm -rf $LIBERRY_ROOT/code;
+cp -r $1/code $LIBERRY_ROOT/;
 pm2 stop app;
-pm2 start app;
+pm2 delete app;
+cd $LIBERRY_ROOT/code/platform;
+pm2 start app.js;
